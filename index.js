77f54fe821b13b71d5ddad1eee7d73083eb95874
Michael Franco <franco.luis.michael@gmail.com>
2022-03-18 10:01:13 -0500

    commit tree 198f4e0a5bd1b31a7dea121cfbb99e2c8fa53251

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+ var customerName = 'bob'; 
+
+ function upperCaseCustomerName(){
+     customerName = customerName.toUpperCase();
+     return customerName;
+
+ }
+
+ function setBestCustomer(){ 
+     bestCustomer = 'not bob';
+
+ }
+
+ function overwriteBestCustomer(){
+     bestCustomer = 'maybe bob';
+ }
+
+const LeastFavoriteCustomer = 'mike';
+function changeLeastFavoriteCustomer(){
+    LeastFavoriteCustomer = 'mike';
+
+}
+
+
+
+//   describe('changeLeastFavoriteCustomer()', function() {
+//     it('unsuccessfully tries to reassign the least favorite customer', function() {
+//       expect(changeLeastFavoriteCustomer).to.throw('Assignment to constant variable.');
+//     });
+//   });
+// });
